74a1e667b57ada351e0f3d0ed1799f0b90dae477
Yves Yuen <yves.j.yuen@gmail.com>
2015-09-24 21:36:49 -0700

    added custom marker

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -2,6 +2,8 @@ app.controller("HomeController", function($scope, $q,$rootScope,$anchorScroll) {
 
   //set this to true to show the app in debugging mode
   $scope.debugging = $rootScope.debugging;
+  var infoWindow; //controller scoped variable
+
 
   $scope.search = function(query) {
     //Hard coded Zenefits' address
@@ -18,7 +20,6 @@ app.controller("HomeController", function($scope, $q,$rootScope,$anchorScroll) {
       mapTypeId: google.maps.MapTypeId.TERRAIN,
       mapTypeControl: false,
     };
-
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
 
@@ -28,7 +29,8 @@ app.controller("HomeController", function($scope, $q,$rootScope,$anchorScroll) {
     $scope.searched = true;
     $scope.markers = [];
     $scope.error = null;
-    // var infoWindow = new google.maps.InfoWindow();
+    $scope.bounds = null;
+    infoWindow = new google.maps.InfoWindow();
   }
 
   function processQuery(query) {
@@ -51,6 +53,18 @@ app.controller("HomeController", function($scope, $q,$rootScope,$anchorScroll) {
     $scope.$digest();
   };
 
+ $scope.triggerClick= (i) => {
+    // if($scope.debugging)
+    //   // console.log($scope.markers[index]);
+      google.maps.event.trigger($scope.markers[i], 'click');
+    // $scope.markers.forEach((marker) => {
+    //   marker.setIcon(defaultIcon);
+    // });
+
+    // $scope.map.setZoom(8);
+    // $scope.map.setCenter($scope.markers[index].getPosition());
+    // $scope.markers[index].setIcon('https://www.google.com/mapfiles/marker_green.png');
+ }
 
   //Function to return only the needed properties
   var trimProp = (results) => {
@@ -70,11 +84,6 @@ app.controller("HomeController", function($scope, $q,$rootScope,$anchorScroll) {
 
   $scope.formatKey = (key) => (key.charAt(0).toUpperCase() + key.substring(1));
 
-
-
-
-
-
   // var request = {
   //   location: zenefits,
   //   radius: '500',
@@ -86,17 +95,18 @@ app.controller("HomeController", function($scope, $q,$rootScope,$anchorScroll) {
   //   console.log(data);
   //   createMarkers(data);
   // });
-  // var createMarker = function (info){
-  //     var marker = new google.maps.Marker({
-  //         map: $scope.map,
-  //         position: new google.maps.LatLng(info.lat, info.long),
-  //         title: info.city
-  //     });
-  //     marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
 
+//orange icon for google map
+var defaultIcon = new google.maps.MarkerImage(
+    "assets/images/pin.png",
+    null, /* size is determined at runtime */
+    null, /* origin is 0,0 */
+    null, /* anchor is bottom center of the scaled image */
+    new google.maps.Size(30, 30)
+);
 
-  //     $scope.markers.push(marker);
-  // }
+var activeIcon = new google.maps.Marker
+
 
   function createMarkers(places) {
     var bounds = new google.maps.LatLngBounds();
@@ -112,82 +122,35 @@ app.controller("HomeController", function($scope, $q,$rootScope,$anchorScroll) {
 
       var marker = new google.maps.Marker({
         map: $scope.map,
-        icon: image,
         title: place.name,
-        position: place.geometry.location
+        index : i+1,
+        html: '<h6>' + (i+1)+ ". " +  place.name + '</h6>',
+        position: place.geometry.location,
+      });
+      marker.setIcon(defaultIcon);
+
+
+
+      // marker.content = '<div class="infoWindowContent">' + place.desc + '</div>';
+      marker.addListener('click', (function(){
+        infoWindow.setContent(this.html);
+        infoWindow.open($scope.map, this);
+        $scope.map.setZoom($scope.zoom + 2);
+        console.log($scope.map);
+        $scope.map.setCenter(this.getPosition());
+      }).bind(marker));
+
+      google.maps.event.addListener(marker, 'mouseover', function() {
+        infoWindow.setContent(this.html);
+        infoWindow.open($scope.map, this);
       });
 
 
-      marker.content = '<div class="infoWindowContent">' + place.desc + '</div>';
-
-      google.maps.event.addListener(marker, 'click', function() {
-        infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-        infoWindow.open(map, marker);
-      });
-
+      $scope.markers.push(marker);
       bounds.extend(place.geometry.location);
     }
     $scope.map.fitBounds(bounds);
+    $scope.bounds = bounds;
+    $scope.zoom = $scope.map.zoom;
   }
-
-
-  // service = new google.maps.places.PlacesService();
-  // $scope.query ="";
-
-  // LocationFactory.searchLocation();
-  // .then(function(location) {
-  // $scope.location = location.coords.accuracy;
-  // console.log(location.coords);
-
-  // })
-  // .then(null,function(err){
-  //   console.log(err);
-  // });
-
-  // $scope.search = function(query) {
-  //   alert(query);
-  //   $scope.location = {
-  //     latitude: LocationFactory.getCurrent().coords.latitude,
-  //     longitude: LocationFactory.getCurrent().coords.longitude
-  //   };
-  //   console.log($scope.location);
-  // };
-  // //
-  // console.log($scope.location);
-
-
-
-  // $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-
-
-  // console.log($scope.map);
-  // $scope.markers = [];
-
-  // var infoWindow = new google.maps.InfoWindow();
-
-  // var createMarker = function(info) {
-
-  //   var marker = new google.maps.Marker({
-  //     map: $scope.map,
-  //     position: new google.maps.LatLng(info.lat, info.long),
-  //     title: info.city
-  //   });
-  //   marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-
-  //   google.maps.event.addListener(marker, 'click', function() {
-  //     infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-  //     infoWindow.open($scope.map, marker);
-  //   });
-
-  //   $scope.markers.push(marker);
-
-  // };
-
-  // for (i = 0; i < cities.length; i++){
-  //     createMarker(cities[i]);
-  // }
-
-
-
 });
